810043becfb7c2b25610964edaba98fe2a3c2e56
Jacqueline Gwynn <jacquelinegwynn@Jacquelines-MacBook-Air.local>
2019-02-02 13:59:57 -0500

    add js

--- assets/javascript/train-scheduler.js
+++ assets/javascript/train-scheduler.js
@@ -1,82 +1,74 @@
-  // Initialize Firebase
-  var config = {
-      apiKey: "AIzaSyBTfOb_Ybwb44m7nfaMBJeZ5xUPO9BDaUQ",
-      authDomain: "train-station-86d38.firebaseapp.com",
-      databaseURL: "https://train-station-86d38.firebaseio.com",
-      projectId: "train-station-86d38",
-      storageBucket: "train-station-86d38.appspot.com",
-      messagingSenderId: "373348168521"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB6kb6WsTntgpMXd8wVRjHv0rY7anqkI9I",
+    authDomain: "new-train-station.firebaseapp.com",
+    databaseURL: "https://new-train-station.firebaseio.com",
+    projectId: "new-train-station",
+    storageBucket: "",
+    messagingSenderId: "748630345318"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
-  var database = firebase.database();
+// initializing variables
+var trainName;
+var destination;
+var trainTime;
+var frequency;
+var tMinutesTillTrain;
+console.log('test')
 
-  // initializing variables
-  var trainName;
-  var destination;
-  var trainTime; 
-  var frequency;
-//   var firstTime;
-  var tMinutesTillTrain;
+$("#Submit").on("click", function () {
+    console.log('clicked the Submit button.');
+    var trainName = $("#train-input").val();
+    var destination = $("#destination-input").val();
+    var trainTime = $("#time-input").val();
+    var frequency = $("#frequency-input").val();
 
-  database.ref().on("child_added", function (snapshot) {
-              console.log(snapshot);
-              trainName = snapshot.val().trainName;
-              destination = snapshot.val().destination;
-              trainTime = snapshot.val().trainTime;
-              frequency = snapshot.val().frequency;
+    console.log(trainName);
+    console.log(destination);
+    console.log(trainTime);
+    console.log(frequency);
 
-            //   var firstTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
-              var firstTimeConverted = moment(); 
-              console.log("FIRST TIME: " + moment(trainTime).format("hh:mm"));
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency
+    })
+})
 
-              var currentTime = moment();
-              console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+database.ref().on("child_added", function (snapshot) {
+    console.log(snapshot.val());
+    trainName = snapshot.val().trainName;
+    destination = snapshot.val().destination;
+    trainTime = snapshot.val().trainTime;
+    frequency = snapshot.val().frequency;
 
-              var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-              console.log("DIFFERENCE IN TIME: " + diffTime);
+    var firstTimeConverted = moment(trainTime, "HH:mm");
+    console.log("FIRST TIME: " + firstTimeConverted.format("HH:mm"));
 
-              // Time apart (remainder)
-              var tRemainder = diffTime % frequency;
-              console.log("TIME REMAINDER: " + tRemainder);
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
 
-              // Minute Until Train
-              var tMinutesTillTrain = frequency - tRemainder;
-              console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    var diffTime = moment().diff(firstTimeConverted, "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
-              // Next Train
-              var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-              console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    console.log("TIME REMAINDER: " + tRemainder);
 
-              $("#trainInfo").append("<tr><td>" + snapshot.val().trainName + "</td" + "<td>" + snapshot.val().destination + "</td" + "<td>" + frequency + "</td>" + "<td>" + tMinutesTillTrain + "</td>" + "<td>" + nextTrain + " " + "</tr></td>"
+    // Minute Until Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-                  //   newRow = newRow + "<td>" + trainName + "</td>";
-                  //   newRow = newRow + "<td>" + destination + "</td>";
-                  //   newRow = newRow + "<td>" + frequency + "</td>";
-                  //   newRow = newRow + "<td>" + nextTrain + "</td>";
-                  //   newRow = newRow + "<td>" + tMinutesTillTrain + "</td>";
-                  //   newRow = newRow + "</tr>"
-              );
+    // Next Train
+    var nextTrainwithMinutesAdded = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrain = moment(nextTrainwithMinutesAdded).format("hh:mm A")
+    console.log('next train ', nextTrain);
+    console.log("ARRIVAL TIME: " + moment(nextTrainwithMinutesAdded).format("hh:mm"));
 
-          $("#Submit").on("click", function () {
-              console.log('clicked the Submit button.');
-              var train = $("#train-input");
-              var destination = $("#destination-input");
-              var trainTime = $("#time-input");
-              var frequency = $("#frequency-input");
+    $("#trainInfo").append("<tr><td>" + snapshot.val().trainName + "</td>" + "<td>" + snapshot.val().destination + "</td>" + "<td>" + snapshot.val().frequency + "</td>" + "<td>" + nextTrain + "</td>" + "<td>" + tMinutesTillTrain + "</tr></td>"
 
-              console.log(train);
-              console.log(destination);
-              console.log(trainTime);
-              console.log(frequency);
-
-              database.ref().push({
-                  train: trainName, 
-                  destination: destination,
-                  trainTime: trainTime,
-                  frequency: frequency,
-                  //nextTrain: nextTrain,
-                  //tMinutesTillTrain: tMinutesTillTrain
-              })
-          })
-        });
+    );
+});
